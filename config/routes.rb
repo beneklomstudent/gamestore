4734213e14be001d459f8b517c6d4c99d71eac9f
beneklomstudent <89109772+beneklomstudent@users.noreply.github.com>
2022-03-25 19:16:14 +1000

    for release
    final1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
@@ -26,28 +25,3 @@ Rails.application.routes.draw do
 
 end
   
-=======
-require 'sidekiq/web'
-
-Rails.application.routes.draw do
-  devise_for :users
-  resources :listings
-  resources :show
-  resources :new
-  authenticate :user, lambda { |u| u.admin? } do
-    mount Sidekiq::Web => "/sideqik"
-    end
-    
-  root to: 'listings#index'
-  get "/index", to: "listings#index"
-  get "/new", to: "listings#new"
-  get "/edit", to: "listings#edit"
-  get "/show", to: "listings#show"
-  get "/test", to: "listings#test"
-  get "/delete", to: "listings#delete"
-  delete 'listings/:id', to: 'listing#destroy'
-  get "/checkout", to: 'listings#checkout'
-
- 
-end
->>>>>>> Stashed changes
