33bcfc29498e0b89746aed62356a4c5bf2747aa7
Ben Eklom <eklomben@hotmail.com>
2022-04-12 13:17:29 +1000

    puma heroku change

--- config/puma.rb
+++ config/puma.rb
@@ -12,6 +12,7 @@ threads min_threads_count, max_threads_count
 # terminating a worker in development environments.
 #
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+heroku config:set "BUNDLE_DISABLE_EXEC_LOAD"="true"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
