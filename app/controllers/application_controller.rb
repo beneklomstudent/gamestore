2dbf3cab81c7d52830185fd7b9c4eb12f910a6a7
Ben Eklom <eklomben@hotmail.com>
2022-04-14 11:00:04 +1000

    back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,20 +22,20 @@ end
 
 
 # checks to see if there is an active cart in session, if not it creates one, if yes it loads one
-# private
-# def current_cart
-#   if session[:cart_id]
-#     cart = Cart.find_by(:id => session[:cart_id])
-#     if cart.present?
-#       @current_cart = cart
-#     else
-#       session[:cart_id] = nil
-#     end
-#   end
+private
+def current_cart
+  if session[:cart_id]
+    cart = Cart.find_by(:id => session[:cart_id])
+    if cart.present?
+      @current_cart = cart
+    else
+      session[:cart_id] = nil
+    end
+  end
 
-#   if session[:cart_id] == nil
-#     @current_cart = Cart.create
-#     session[:cart_id] = @current_cart.id
+  if session[:cart_id] == nil
+    @current_cart = Cart.create
+    session[:cart_id] = @current_cart.id
     
-#   end
-# end
+  end
+end
